de8ce172b145de819145d5c85199f394f3ea3a4c
anto-kazungu <kazoga.antony@gmail.com>
2022-11-05 16:55:47 +0300

    updated db url

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres22
-export POSTGRES_HOST=postgres.cdifaileaais.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgress22
+export POSTGRES_HOST=micro-db.cfpuhio8577b.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=micro-project
 export AWS_REGION=us-east-1
