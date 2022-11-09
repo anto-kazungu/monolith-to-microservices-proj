9d53dbee4dbed0ce9727e7fa7742e25f0612366d
anto-kazungu <kazoga.antony@gmail.com>
2022-11-09 17:02:13 +0300

    updated files and created kubernets

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+kubectl delete deploy backend-user
+kubectl delete deploy backend-feed
+kubectl delete deploy reverseproxy
+kubectl delete deploy frontend
+
+kubectl apply -f feed-deployment.yaml
+kubectl apply -f user-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
